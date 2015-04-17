9aae76e14a99e750b6429d160dfbc3b05fd96724
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-04-17 16:14:58 -0400

    new ladning page in orange

--- assets/themes/air-traffic-control/lib/template/metabox-landingpage.php
+++ assets/themes/air-traffic-control/lib/template/metabox-landingpage.php
@@ -4,7 +4,7 @@
 $postid = is_admin()?$_GET['post']:$post->ID;
 $template_file = get_post_meta($postid,'_wp_page_template',TRUE);
   // check for a template type
-if (is_admin() && ($template_file == 'front-page.php' || stristr($template_file,'landing-page') || $post->ID == get_option('page_on_front'))) { ?>
+if (is_admin() && ($template_file == 'front-page.php' || $template_file == 'landing-page.php' || $post->ID == get_option('page_on_front'))) { ?>
 <style>
     .landingpage_meta_control .table {display: block; width: 100%;}
     .landingpage_meta_control .row {display: block;cursor: move;border-bottom: 1px solid #333;}
